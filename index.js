273e5a6cba96a0bbcc95c519eb36b7e0095ced2b
Sunkissedd <100358110+Sunkissedd@users.noreply.github.com>
2022-02-24 13:01:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dbd = require("dbd.js")
 
 const bot = new dbd.Bot({
   mobile: false, // you can change it to "true" if you want a mobile status
-  token: "TOKEN", // change TOKEN with your bot token
+  token: "OTQ2MDM2MjI2ODgyOTMyNzg2.YhY25A.CMkaw3nkzUwwywaFmJFtj96fiGI", // change TOKEN with your bot token
   prefix: ['$getServerVar[prefix]', '<@!$clientID>', '<@$clientID>'] // you may change the bot's main prefix in vars.js
 })
 
